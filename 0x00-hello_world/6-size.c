daff140ed079776277edbec042aa0ed24f1ac0e7
ThePurpleAlpha <ojopeaceayobami.9@gmail.com>
2023-04-20 14:53:00 -0700

    alx

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,13 +3,13 @@
  * main - A program that prints the sizes of various type on the computer types
  * Return: 0 (Success)
  */
- int main(void)
-{       
-        char a;
-        int b;
-        long int c;
-        long long int d;
-        float f;
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float f;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
