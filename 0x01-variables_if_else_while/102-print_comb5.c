74239edb6f0d6564cdbeae280ccd4e12ebaf563e
ThePurpleAlpha <ojopeaceayobami.9@gmail.com>
2023-04-21 16:17:48 -0700

    alx

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,21 +3,26 @@
  * main - prints all possible combination of two digit number
  * Return: Always 0 (Success)
  */
-int main() {
+int main(void) 
+{
 int i, j;
 
-for (i = 0; i <= 99; i++) {
-for (j = i; j <= 99; j++) {
+for (i = 0; i <= 99; i++) 
+{
+for (j = i; j <= 99; j++) 
+{
 putchar(i / 10 + '0');
 putchar(i % 10 + '0');
 putchar(' ');
 putchar(j / 10 + '0');
 putchar(j % 10 + '0');
-if (i != 99 || j != 99) {
+if (i != 99 || j != 99) 
+{
 putchar(',');
 putchar(' ');
 }
 }
 }
+putchar('\n');
 return 0;
 }
