8d8eac10e29b4c7ec6d2a5752b9086770231a7ab
ThePurpleAlpha <ojopeaceayobami.9@gmail.com>
2023-04-22 01:29:10 -0700

    alx

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,7 +7,7 @@ int main(void)
 {
 int i, j;
 
-for (i = 0; i <= 99; i++)
+for (i = 0; i <= 98; i++)
 {
 for (j = 0 + 1; j <= 99; j++)
 	{
@@ -16,7 +16,8 @@ putchar(i % 10 + '0');
 putchar(' ');
 putchar(j / 10 + '0');
 putchar(j % 10 + '0');
-if (i != 99 || j != 99)
+if (i == 98 && j == 99
+		continue;)
 {
 putchar(',');
 putchar(' ');
