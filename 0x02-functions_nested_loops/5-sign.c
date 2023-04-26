469e39383165b2a384244d4b811354af6ff79ebe
ThePurpleAlpha <ojopeaceayobami.9@gmail.com>
2023-04-26 06:31:00 -0700

    alx

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_sign - prints the sign of a number
@@ -17,10 +17,10 @@ return (1);
 }
 else if (n == 0)
 {
-_putchar('0');
+_putchar('48');
 return (0);
 }
-else
+else if (n < 0)
 {
 _putchar('-');
 return (-1);
