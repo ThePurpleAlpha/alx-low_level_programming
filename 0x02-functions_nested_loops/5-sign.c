78be3eba45e1e6b1b132177a45d4b766f94bb565
ThePurpleAlpha <ojopeaceayobami.9@gmail.com>
2023-04-25 17:25:04 -0700

    alx

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,12 +8,12 @@
  *         0 and prints 0 if n is zero,
  *         -1 and prints - if n is less than zero
  */
-int print_sign(int n);
+int print_sign(int n)
 {
 if (n > 0)
 {
 putchar('+');
-return (1);
+return (+1);
 }
 else if (n == 0)
 {
