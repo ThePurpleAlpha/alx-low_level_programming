466bb233171b5f54d11a6e14b1b30d6996c952e8
ThePurpleAlpha <ojopeaceayobami.9@gmail.com>
2023-04-26 00:42:56 -0700

    alx

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 98 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int i;
+unsigned long int fib1 = 1, fib2 = 2, fib3;
+
+printf("%lu, %lu", fib1, fib2);
+
+for (i = 2; i < 98; i++)
+{
+fib3 = fib1 + fib2;
+printf(", %lu", fib3);
+
+fib1 = fib2;
+fib2 = fib3;
+}
+
+printf("\n");
+return (0);
+}
