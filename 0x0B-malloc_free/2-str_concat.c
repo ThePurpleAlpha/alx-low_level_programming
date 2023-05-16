864b25ea8c93050e272cf4ffb738e3254863c784
ThePurpleAlpha <ojopeaceayobami.9@gmail.com>
2023-05-16 05:07:07 -0700

    alx

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,7 +13,7 @@ char *str_concat(char *s1, char *s2)
 {
 	char *conct;
 
-	int i, ci;
+	int i=0, ci=0;
 
 	if (s1 == NULL)
 
@@ -39,7 +39,6 @@ char *str_concat(char *s1, char *s2)
 
 		return (NULL);
 
-	i = ci = 0;
 
 	while (s1[i] != '\0')
 
