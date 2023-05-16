462c5282d232faafcf5b2aaba15eff626f6174f3
ThePurpleAlpha <ojopeaceayobami.9@gmail.com>
2023-05-16 05:55:27 -0700

    alx

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -13,7 +14,7 @@ char *str_concat(char *s1, char *s2)
 {
 	char *conct;
 
-	int i, ci;
+	int i=0, ci=0;
 
 	if (s1 == NULL)
 
