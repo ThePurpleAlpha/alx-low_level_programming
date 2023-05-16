74daf872ebea96e8b3344f8abe21535c390080fc
ThePurpleAlpha <ojopeaceayobami.9@gmail.com>
2023-05-16 03:59:40 -0700

    alx

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,14 +5,10 @@
 #include "main.h"
 
 /**
-
- * _strdup - duplicate to new memory space location
-
- * @str: char
-
- * Return: 0
-
- */
+* _strdup - duplicate to new memory space location
+* @str: char
+* Return: 0
+*/
 
 char *_strdup(char *str)
 
@@ -20,10 +16,9 @@ char *_strdup(char *str)
 	char *aaa;
 
 	int i, r = 0;
-		
+
 	if (str == NULL)
 		return (NULL);
-
 	i = 0;
 
 	while (str[i] != '\0')
